c9852a36065ab81ee490b52d5398425418dd12e5
Flozzie443 <owinoflorence443@gmail.com>
2025-07-13 13:06:09 -0700

    Add script to convert argument to integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -4,5 +4,8 @@ const num = parseInt(arg);
 if (!isNaN(num)) {
   console.log(`My number: ${num}`);
 } else {
-  console.log(''89'');
-}
+  console.log('89');
+}console.log('89');
+console.log('-4545');
+console.log('-4545');
+console.log('ALX');
